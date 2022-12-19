e78273ab9ad057c736b7dbee6a43f7b61cb7f3c7
Jess <jessicadixie14@hotmail.com>
2022-12-18 20:25:47 -0800

    Worked on keypresses, including calculating X properly

--- app.js
+++ app.js
@@ -26,20 +26,25 @@ btns.forEach(btn => {
         if(screen.textContent === '0' && e.target.getAttribute('data-operator') != '=') {
             clearScreen();
         }
+
+        //Get operator for equation
         if(e.target.getAttribute('data-operator') != '=' && e.target.getAttribute('data-misc') != 'del') {
+            // Stop multiple operators from being added to the end
             if (/[+-\/*]/.test(e.target.getAttribute('data-operator')) && /[+-\/*]$/.test(screen.textContent)) {
                 screen.textContent = screen.textContent.slice(0, -1);
                 operator = e.target.getAttribute('data-operator');
             }
             screen.textContent += btn.textContent;
         }
+
         // Calculate X
         if(e.target.getAttribute('data-operator') && x == null && screen.textContent != '') {
             currentNum = Number(screen.textContent.replace(/[^0-9.]+/g, ""));
             x = currentNum;
             dotCount = 0;
+
         // Calculate Y
-        } else if (e.target.getAttribute('data-operator') && y == null && res == null) {
+        } else if (e.target.getAttribute('data-operator') && y == null && res == null && !!operator) {
             if(screen.textContent.includes(operator) && Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, "")) != 0 || !!Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, ""))) {
                 y = Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, ""));
                 dotCount = 0;
@@ -48,7 +53,7 @@ btns.forEach(btn => {
 
         if(e.target.getAttribute('data-operator') == '=' && x !== null && y !== null) {
             res = calc(x, y);
-            if(res > 9999999999) {
+            if(res > 99999999) {
                 screen.textContent = res.toExponential();
             } else if(res % 1 !== 0) {
                 screen.textContent = res.toFixed(2);
@@ -68,14 +73,56 @@ btns.forEach(btn => {
 document.addEventListener("keydown", e => {
     let keypress = e.key;
     for(let btn of btns) {
+        if(keypress === "Backspace" || keypress === "Delete") {
+            return screen.textContent = screen.textContent.slice(0, -1);
+        }
         if(keypress === btn.textContent) {
+            if(screen.textContent === '0') {
+                clearScreen();
+            }
             if(keypress === '.' && dotCount > 0) {
                 return;
             } else if (keypress === '.'){
                 dotCount++;
                 return screen.textContent += keypress;
             }
-            screen.textContent += keypress;
+            // Calculate X
+            if(x == null && screen.textContent != '' && keypress === '+' || keypress === '-' || keypress === '/' || keypress === '*') {
+                currentNum = Number(screen.textContent.replace(/[^0-9.]+/g, ""));
+                x = currentNum;
+                dotCount = 0;
+
+        // Calculate Y
+         } else if (y == null && res == null && !!operator) {
+            if(screen.textContent.includes(operator) && Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, "")) != 0 || !!Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, ""))) {
+                y = Number(screen.textContent.substring(screen.textContent.indexOf(operator) + 1).replace(/[^0-9.]+/g, ""));
+                dotCount = 0;
+            } 
+        }
+        if(keypress === '+' || keypress === '-' || keypress === '/' || keypress === '*') {
+            if (/[+-\/*]/.test(keypress) && /[+-\/*]$/.test(screen.textContent)) {
+                screen.textContent = screen.textContent.slice(0, -1);
+            }
+            operator = keypress;
+        }
+            if(keypress === 'Enter' && !!x && !!y && !!operator) {
+                res = calc(x, y);
+                if(res > 9999999999) {
+                    screen.textContent = res.toExponential();
+                } else if(res % 1 !== 0) {
+                    screen.textContent = res.toFixed(2);
+                } else {
+                    screen.textContent = res;
+                }
+                currentNum = res;
+                x = res;
+                y = null;
+                res = null;
+                operator = null;
+            }
+            if(keypress != '=' && keypress !== 'Backspace') {
+                screen.textContent += keypress;
+            }
         }
     }
 })
